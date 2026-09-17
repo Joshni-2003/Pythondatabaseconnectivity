2220a5c729fcf9ca20d235aafcb935d8d358a40b
Joshni-2003 <joshnithota@gmail.com>
2026-09-17 15:42:29 +0530

    Add files via upload

---
+++ pdbc.sql
@@ -0,0 +1,4 @@
+create database pdbc;
+use pdbc;
+create table employeemanagementsystem(id int primary key auto_increment,name varchar(50),salary decimal(10,2),dept varchar(50),location varchar(50));
+select * from employeemanagementsystem;
